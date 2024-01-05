ed3092ecc4a47529c88dfb052e251c5cbd16c69d
Paul Steneram Bibby <me@paulstenerambibby.se>
2024-01-05 16:54:36 +0100

    Add test for getAllChores

--- app/src/main/java/stoneframe/chorelist/ChoreList.java
+++ app/src/main/java/stoneframe/chorelist/ChoreList.java
@@ -1,5 +1,14 @@
 package stoneframe.chorelist;
 
+import java.util.Collections;
+import java.util.List;
+
+import stoneframe.chorelist.model.Chore;
+
 public class ChoreList
 {
+    public List<Chore> getAllChores()
+    {
+        return Collections.emptyList();
+    }
 }
--- app/src/test/java/stoneframe/chorelist/ChoreListTest.java
+++ app/src/test/java/stoneframe/chorelist/ChoreListTest.java
@@ -1,12 +1,25 @@
 package stoneframe.chorelist;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import stoneframe.chorelist.model.Chore;
 
 public class ChoreListTest
 {
     @Test
-    public void initializeChoreList()
+    public void GetAllChores_NoChoresAdded_ReturnEmptyList()
     {
-        new ChoreList();
+        ChoreList choreList = new ChoreList();
+
+        List<Chore> allChores = choreList.getAllChores();
+
+        assertEquals(Collections.emptyList(), allChores);
     }
 }

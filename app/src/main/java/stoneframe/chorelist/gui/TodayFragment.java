b82306191d9a71b28f06dd3ca30145b31835e647
Paul Steneram Bibby <me@paulstenerambibby.se>
2024-01-11 22:19:41 +0100

    Add animation when finishing chore and task

--- app/src/main/java/stoneframe/chorelist/gui/TodayFragment.java
+++ app/src/main/java/stoneframe/chorelist/gui/TodayFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
@@ -35,20 +36,31 @@ public class TodayFragment extends Fragment
             .getApplication();
         choreList = globalState.getChoreList();
 
-        View view = inflater.inflate(R.layout.fragment_today, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_today, container, false);
 
         choreAdapter = new ArrayAdapter<>(
             getActivity().getBaseContext(),
             android.R.layout.simple_list_item_checked);
-        ListView choreListView = view.findViewById(R.id.todays_chores);
+        ListView choreListView = rootView.findViewById(R.id.todays_chores);
         choreListView.setAdapter(choreAdapter);
-        choreListView.setOnItemClickListener((parent, view1, position, id) ->
+        choreListView.setOnItemClickListener((parent, view, position, id) ->
         {
-            Chore chore = choreAdapter.getItem(position);
-            choreList.choreDone(chore);
-            choreAdapter.remove(chore);
+            CheckedTextView checkedTextView = (CheckedTextView)view;
+
+            checkedTextView.setChecked(true);
+
+            new Thread(() ->
+                getActivity().runOnUiThread(() ->
+                {
+                    waitOneSecond();
+                    Chore chore = choreAdapter.getItem(position);
+                    choreList.choreDone(chore);
+                    choreAdapter.remove(chore);
+                    checkedTextView.setChecked(false);
+                })).start();
+
         });
-        choreListView.setOnItemLongClickListener((parent, view12, position, id) ->
+        choreListView.setOnItemLongClickListener((parent, view, position, id) ->
         {
             Chore chore = choreList.getTodaysChores().get(position);
             choreList.choreSkip(chore);
@@ -60,24 +72,26 @@ public class TodayFragment extends Fragment
         taskAdapter = new ArrayAdapter<>(
             getActivity().getBaseContext(),
             android.R.layout.simple_list_item_checked);
-        ListView taskListView = view.findViewById(R.id.todays_tasks);
+        ListView taskListView = rootView.findViewById(R.id.todays_tasks);
         taskListView.setAdapter(taskAdapter);
-        taskListView.setOnItemClickListener((parent, view1, position, id) ->
+        taskListView.setOnItemClickListener((parent, view, position, id) ->
         {
-            try
-            {
-                TimeUnit.SECONDS.sleep(1);
-            }
-            catch (InterruptedException ie)
-            {
-                Thread.currentThread().interrupt();
-            }
-            Task task = taskAdapter.getItem(position);
-            choreList.taskDone(task);
-            taskAdapter.remove(task);
+            CheckedTextView checkedTextView = (CheckedTextView)view;
+
+            checkedTextView.setChecked(true);
+
+            new Thread(() ->
+                getActivity().runOnUiThread(() ->
+                {
+                    waitOneSecond();
+                    Task task = taskAdapter.getItem(position);
+                    choreList.taskDone(task);
+                    taskAdapter.remove(task);
+                    checkedTextView.setChecked(false);
+                })).start();
         });
 
-        return view;
+        return rootView;
     }
 
     @Override
@@ -97,5 +111,17 @@ public class TodayFragment extends Fragment
 
         choreAdapter.clear();
         taskAdapter.clear();
+    }
+
+    private static void waitOneSecond()
+    {
+        try
+        {
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch (InterruptedException ie)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }

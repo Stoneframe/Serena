244a6d3a4c0908ba49a5e175a25d7a8fb2f59848
Paul Steneram Bibby <me@paulstenerambibby.se>
2024-07-10 21:04:57 +0200

    Close keyboard when leaving Calories fragment

--- app/src/main/java/stoneframe/chorelist/gui/CaloriesFragment.java
+++ app/src/main/java/stoneframe/chorelist/gui/CaloriesFragment.java
@@ -1,10 +1,12 @@
 package stoneframe.chorelist.gui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -64,6 +66,15 @@ public class CaloriesFragment extends Fragment
             new EditTextCriteria(editTextCalories, EditTextCriteria.IS_VALID_INT));
 
         return view;
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+
+        InputMethodManager imm = (InputMethodManager)requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(requireView().getWindowToken(), 0);
     }
 
     private void updateAvailableCalories()

50fdd05e50c35dd66755610ec8b36c85e0dee599
Paul Steneram Bibby <me@paulstenerambibby.se>
2024-11-03 15:02:42 +0100

    Fix WeekRoutineTest

--- app/src/test/java/stoneframe/serena/WeekRoutineTest.java
+++ app/src/test/java/stoneframe/serena/WeekRoutineTest.java
@@ -1,177 +1,308 @@
-//package stoneframe.chorelist;
-//
-//import static junit.framework.TestCase.assertEquals;
-//import static junit.framework.TestCase.assertTrue;
-//
-//import org.joda.time.DateTimeConstants;
-//import org.joda.time.LocalDateTime;
-//import org.joda.time.LocalTime;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.List;
-//
-//import stoneframe.chorelist.model.routines.PendingProcedure;
-//import stoneframe.chorelist.model.routines.Procedure;
-//import stoneframe.chorelist.model.routines.WeekRoutine;
-//
-//public class WeekRoutineTest
-//{
-//    private final LocalDateTime now = new LocalDateTime(2024, 1, 1, 0, 0);
-//
-//    private WeekRoutine routine;
-//
-//    @Before
-//    public void before()
-//    {
-//        routine = new WeekRoutine("Week Routing", now);
-//    }
-//
-//    @Test
-//    public void getNextProcedureTime_Monday10AndNowIsDay1_returnCorrectTime()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure", new LocalTime(10, 0)));
-//
-//        LocalDateTime next = routine.getNextProcedureTime(now);
-//
-//        assertEquals(new LocalDateTime(2024, 1, 1, 10, 0), next);
-//    }
-//
-//    @Test
-//    public void getNextProcedureTime_Monday10AndNowIsDay1Hour12_returnCorrectTime()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure", new LocalTime(10, 0)));
-//
-//        LocalDateTime next = routine.getNextProcedureTime(now.plusHours(12));
-//
-//        assertEquals(new LocalDateTime(2024, 1, 8, 10, 0), next);
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_NoProcedures_returnEmptyList()
-//    {
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now);
-//
-//        assertTrue(pendingProcedures.isEmpty());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday10AndNowIsDay1Hour0_returnEmptyList()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure", new LocalTime(10, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now);
-//
-//        assertTrue(pendingProcedures.isEmpty());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday10AndNowIsDay2Hour0_listContainsMonday10Procedure()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure", new LocalTime(10, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(2));
-//
-//        assertEquals(1, pendingProcedures.size());
-//        assertEquals("Procedure", pendingProcedures.get(0).getDescription());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday10AndNowIsDay1Hour10_listContainsMonday10Procedure()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure", new LocalTime(10, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusHours(10));
-//
-//        assertEquals(1, pendingProcedures.size());
-//        assertEquals("Procedure", pendingProcedures.get(0).getDescription());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday15AndFriday23NowIsDay3Hour0_listContainsMonday10Procedure()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure 1", new LocalTime(15, 0)));
-//
-//        routine.getWeek().getWeekDay(DateTimeConstants.FRIDAY)
-//            .addProcedure(new Procedure("Procedure 2", new LocalTime(23, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(3));
-//
-//        assertEquals(1, pendingProcedures.size());
-//        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday15AndFriday23NowIsDay6Hour0_listContainsMonday10AndFriday23Procedures()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure 1", new LocalTime(15, 0)));
-//
-//        routine.getWeek().getWeekDay(DateTimeConstants.FRIDAY)
-//            .addProcedure(new Procedure("Procedure 2", new LocalTime(23, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(6));
-//
-//        assertEquals(2, pendingProcedures.size());
-//        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
-//        assertEquals("Procedure 2", pendingProcedures.get(1).getDescription());
-//    }
-//
-//    @Test
-//    public void getPendingProcedures_Monday15AndFriday23NowIsDay9Hour0_listContainsFriday23AndMonday10Procedures()
-//    {
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY)
-//            .addProcedure(new Procedure("Procedure 1", new LocalTime(15, 0)));
-//
-//        routine.getWeek().getWeekDay(DateTimeConstants.FRIDAY)
-//            .addProcedure(new Procedure("Procedure 2", new LocalTime(23, 0)));
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(9));
-//
-//        assertEquals(3, pendingProcedures.size());
-//        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
-//        assertEquals("Procedure 2", pendingProcedures.get(1).getDescription());
-//        assertEquals("Procedure 1", pendingProcedures.get(2).getDescription());
-//    }
-//
-//    @Test
-//    public void procedureDone_Monday10IsDoneAndNowIsDay2Hour0_listIsEmpty()
-//    {
-//        Procedure procedure = new Procedure("Procedure", new LocalTime(10, 0));
-//
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY).addProcedure(procedure);
-//
-//        PendingProcedure pendingProcedure = routine.getPendingProcedure(now.plusDays(2));
-//
-//        routine.procedureDone(pendingProcedure);
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(2));
-//
-//        assertTrue(pendingProcedures.isEmpty());
-//    }
-//
-//    @Test
-//    public void procedureDone_Monday10IsDoneAndTuesday15AndNowIsDay3Hour0_listIsEmpty()
-//    {
-//        Procedure procedure1 = new Procedure("Procedure 1", new LocalTime(10, 0));
-//        Procedure procedure2 = new Procedure("Procedure 2", new LocalTime(15, 0));
-//
-//        routine.getWeek().getWeekDay(DateTimeConstants.MONDAY).addProcedure(procedure1);
-//        routine.getWeek().getWeekDay(DateTimeConstants.TUESDAY).addProcedure(procedure2);
-//
-//        PendingProcedure pendingProcedure = routine.getPendingProcedure(now.plusDays(2));
-//
-//        routine.procedureDone(pendingProcedure);
-//
-//        List<PendingProcedure> pendingProcedures = routine.getPendingProcedures(now.plusDays(2));
-//
-//        assertEquals(1, pendingProcedures.size());
-//        assertEquals("Procedure 2", pendingProcedures.get(0).getDescription());
-//    }
-//}
+package stoneframe.serena;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import stoneframe.serena.mocks.TestContext;
+import stoneframe.serena.model.routines.PendingProcedure;
+import stoneframe.serena.model.routines.Procedure;
+import stoneframe.serena.model.routines.RoutineManager;
+import stoneframe.serena.model.routines.WeekRoutine;
+import stoneframe.serena.model.routines.WeekRoutineEditor;
+
+public class WeekRoutineTest
+{
+    private final LocalDate today = new LocalDate(2024, 1, 1);
+
+    private TestContext context;
+
+    private RoutineManager routineManager;
+
+    @Before
+    public void before()
+    {
+        context = new TestContext().setCurrentTime(today);
+
+        routineManager = context.getRoutineManager();
+    }
+
+    @Test
+    public void getNextProcedureTime_getNextBeforeTimeOfProcedure_returnTimeOfProcedure()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today);
+
+        LocalDateTime nextProcedureTime = routineManager.getNextProcedureTime();
+
+        // ASSERT
+        assertEquals(today.toLocalDateTime(new LocalTime(10, 0)), nextProcedureTime);
+    }
+
+    @Test
+    public void getNextProcedureTime_getNextAfterTimeOfProcedure_returnTimeOfProcedureNextWeek()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.toLocalDateTime(new LocalTime(12, 0)));
+
+        LocalDateTime nextProcedureTime = routineManager.getNextProcedureTime();
+
+        // ASSERT
+        assertEquals(today.plusWeeks(1).toLocalDateTime(new LocalTime(10, 0)), nextProcedureTime);
+    }
+
+    @Test
+    public void getPendingProcedures_noProceduresAdded_returnEmptyList()
+    {
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        assertTrue(pendingProcedures.isEmpty());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingBeforeTimeOfProcedure_returnEmptyList()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today);
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertTrue(pendingProcedures.isEmpty());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingTwoDaysAfterProcedureAdded_listContainsProcedure()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(2));
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertEquals(1, pendingProcedures.size());
+        assertEquals("Procedure", pendingProcedures.get(0).getDescription());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingAtTimeOfProcedureAdded_listContainsProcedure()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.toLocalDateTime(new LocalTime(10, 0)));
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertEquals(1, pendingProcedures.size());
+        assertEquals("Procedure", pendingProcedures.get(0).getDescription());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingBetweenTimesOfTwoAddedProcedures_listContainsFirstProcedure()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure 1", new LocalTime(15, 0)));
+
+        routineEditor.addProcedure(
+            DateTimeConstants.FRIDAY,
+            new Procedure("Procedure 2", new LocalTime(23, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(3));
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertEquals(1, pendingProcedures.size());
+        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingAfterTwoAddedProcedures_listContainsBothProcedures()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure 1", new LocalTime(15, 0)));
+
+        routineEditor.addProcedure(
+            DateTimeConstants.FRIDAY,
+            new Procedure("Procedure 2", new LocalTime(23, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(6).toLocalDateTime(new LocalTime(0, 0)));
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertEquals(2, pendingProcedures.size());
+        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
+        assertEquals("Procedure 2", pendingProcedures.get(1).getDescription());
+    }
+
+    @Test
+    public void getPendingProcedures_getPendingAfterTwoProceduresAndTheFirstAgain_listContainsBothProceduresAndFirstTwice()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure 1", new LocalTime(15, 0)));
+
+        routineEditor.addProcedure(
+            DateTimeConstants.FRIDAY,
+            new Procedure("Procedure 2", new LocalTime(23, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(9));
+
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        // ASSERT
+        assertEquals(3, pendingProcedures.size());
+        assertEquals("Procedure 1", pendingProcedures.get(0).getDescription());
+        assertEquals("Procedure 2", pendingProcedures.get(1).getDescription());
+        assertEquals("Procedure 1", pendingProcedures.get(2).getDescription());
+    }
+
+    @Test
+    public void procedureDone_markPendingProcedureAsDone_getPendingProceduresIsEmpty()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure", new LocalTime(10, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(2));
+
+        PendingProcedure pendingProcedure = routineManager.getPendingProcedures().get(0);
+
+        routineManager.procedureDone(pendingProcedure);
+
+        // ASSERT
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        assertTrue(pendingProcedures.isEmpty());
+    }
+
+    @Test
+    public void procedureDone_markFirstPendingProcedureAsDone_getPendingProceduresContainsSecondPendingProcedure()
+    {
+        // ARRANGE
+        WeekRoutine routine = routineManager.createWeekRoutine();
+
+        WeekRoutineEditor routineEditor = routineManager.getWeekRoutineEditor(routine);
+
+        routineEditor.addProcedure(
+            DateTimeConstants.MONDAY,
+            new Procedure("Procedure 1", new LocalTime(10, 0)));
+
+        routineEditor.addProcedure(
+            DateTimeConstants.TUESDAY,
+            new Procedure("Procedure 2", new LocalTime(15, 0)));
+
+        routineEditor.save();
+
+        // ACT
+        context.setCurrentTime(today.plusDays(2));
+
+        PendingProcedure pendingProcedure = routineManager.getPendingProcedures().get(0);
+
+        routineManager.procedureDone(pendingProcedure);
+
+        // ASSERT
+        List<PendingProcedure> pendingProcedures = routineManager.getPendingProcedures();
+
+        assertEquals(1, pendingProcedures.size());
+        assertEquals("Procedure 2", pendingProcedures.get(0).getDescription());
+    }
+}
